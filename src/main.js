be2a04fcccbf16634527b1f07d960fae0fe88eba
Shystriyhlopchik <Shystriyhlopchik@yandex.ru>
2020-06-11 14:44:31 +0300

    добавил валлидацию регистрации

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter.js'
+import messagePlugin from '@/utils/message.plugin.js'
 import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
+
+Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
