e6b50b03c507b06fe6be624e891de5d247615539
Shystriyhlopchik <Shystriyhlopchik@yandex.ru>
2020-06-15 13:20:48 +0300

    Добавил редактирование категорий

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -6,8 +6,16 @@
   <section>
     <Loader v-if="loading" />
     <div class="row" v-else>
+
       <CategoryCreate @created="addNewCategory" />
-      <CategoryEdit />
+
+      <CategoryEdit 
+        v-if="categories.length"
+        :categories="categories"
+        :key="categories.length + updateCount" 
+        @updated="updateCategories"
+      />
+      <p v-else class="center">Категорий пока нет</p>
     </div>
   </section>
 </div>
@@ -21,7 +29,8 @@ export default {
   name: 'categories',
   data: () => ({
     categories: [],
-    loading: true
+    loading: true,
+    updateCount: 0
   }),
   async mounted() {
     this.categories = await this.$store.dispatch('fetchCategories')
@@ -33,6 +42,12 @@ export default {
   methods: {
     addNewCategory(category) {
       this.categories.push(category)
+    },
+    updateCategories(category) {
+      const idx = this.categories.findIndex(c => c.id === category.id)
+      this.categories[idx].title = category.title
+      this.categories[idx].limit = category.limit
+      this.updateCount++
     }
   }
 }
